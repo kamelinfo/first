90922ce74b5c64b06b24f6164d987374bb6d6b1c
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2024-12-22 13:24:20 +0100

    aaa

---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Role extends Model
+{
+    use HasFactory;
+    function users()  {
+        return  $this->belongsToMany(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    function roles()  {
+        return  $this->belongsToMany(Role::class);
+    }
 }
